a4bd5f554632e57dd1071db02b6b6a837458a91f
eudp-admin <infoeudp2017@gmail.com>
2025-10-03 10:17:04 +0300

    Update settings.py

--- party_management/settings.py
+++ party_management/settings.py
@@ -194,6 +194,9 @@ else:
     # --- DEVELOPMENT SETTINGS ---
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'development@example.com'
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
 
 # Crispy Forms Settings
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
